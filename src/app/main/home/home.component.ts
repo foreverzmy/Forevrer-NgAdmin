2cb6b10abe91889e4618fa11ca3324d6c28ba92d
trc-forever <921255465@qq.com>
2017-08-24 15:23:52 +0800

    build

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -7,7 +7,7 @@ import { PageRoutes } from '../../page/page.routes';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  private menus;
+  public menus;
 
   constructor() { }
 
